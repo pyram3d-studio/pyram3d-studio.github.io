e383e656c050f48ac28fe74072511fe4b91f29a1
Svenja Reissaus <alvaro.pena@itproaser.com>
2021-06-17 10:30:34 -0500

    Keops site

---
+++ pages/keops.tsx
@@ -0,0 +1,11 @@
+export default function Keops() {
+    return (
+        <div className="">
+            <header>
+                <h2>
+                    Pirámide Keops
+                </h2>
+            </header>
+        </div>
+    )
+}
